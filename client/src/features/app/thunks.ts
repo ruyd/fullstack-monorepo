811fda93b7b525711669c8c1ab59d1de083da599
RD <ruydelgado@gmail.com>
2022-06-29 20:53:25 -0500

    refine: thunks

--- client/src/features/app/thunks.ts
+++ client/src/features/app/thunks.ts
@@ -3,25 +3,30 @@ import axios, { AxiosResponse } from 'axios'
 import { AppUser, decodeUser, onLogin } from '../../shared/auth'
 import { notify, notifyError, patch } from './slice'
 
-const options = {
-  validateStatus: () => true,
-}
-
+/**
+ * Axios wrapper for thunks that picks up the bearer token from auth.setHeader
+ * @param dispatch
+ * @param url
+ * @param data
+ * @param method
+ * @returns
+ */
 export async function request(
   dispatch: ThunkDispatch<unknown, unknown, AnyAction>,
   url: string,
-  data: any
+  data: any,
+  method: string = 'post'
 ): Promise<AxiosResponse> {
   let response: AxiosResponse
   try {
     dispatch(patch({ loading: true }))
-    response = await axios({ ...options, url, data, method: 'post' })
-    if (response.status !== 200) {
-      dispatch(notifyError(response.data.message))
-      throw new Error(response.statusText)
-    }
+    response = await axios({
+      url,
+      data,
+      method,
+    })
   } catch (error: any) {
-    dispatch(notifyError(error.message))
+    dispatch(notifyError(error?.response?.data?.message || error.message))
     throw error
   } finally {
     dispatch(patch({ loading: false }))
@@ -29,18 +34,25 @@ export async function request(
   return response
 }
 
+function setLogin(
+  dispatch: ThunkDispatch<unknown, unknown, AnyAction>,
+  token: string
+) {
+  const user = decodeUser(token) as AppUser
+  dispatch(
+    patch({
+      token,
+      user,
+    })
+  )
+  onLogin(token)
+}
+
 export const LoginAsync = createAsyncThunk(
   'app/login',
   async (payload: { email: string; password: string }, { dispatch }) => {
     const response = await request(dispatch, 'profile/login', payload)
-    dispatch(
-      patch({
-        token: response.data.token,
-        user: response.data.user,
-        loading: false,
-      })
-    )
-    onLogin(response.data.token)
+    setLogin(dispatch, response.data.token)
   }
 )
 
@@ -48,14 +60,7 @@ export const RegisterAsync = createAsyncThunk(
   'app/register',
   async (payload: unknown, { dispatch }) => {
     const response = await request(dispatch, 'profile/register', payload)
-    const user = decodeUser(response.data.token) as AppUser
-    dispatch(
-      patch({
-        token: response.data.token,
-        user,
-        loading: false,
-      })
-    )
-    onLogin(response.data.token)
+    //For email validation rework this
+    setLogin(dispatch, response.data.token)
   }
 )
