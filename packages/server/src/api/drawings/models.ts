12783792a3cb48165a838d7df90ff6c4fa79f9e9
RD <ruydelgado@gmail.com>
2022-07-12 02:37:25 -0500

    idea

--- packages/server/src/api/drawings/models.ts
+++ packages/server/src/api/drawings/models.ts
@@ -1,6 +1,36 @@
 import { Drawing } from '@root/lib'
-import { Model, DataTypes } from 'sequelize'
+import { Model, DataTypes, ModelAttributes, Attributes } from 'sequelize'
 import db, { commonOptions } from '../../shared/db'
+
+export interface ModelConfig {
+  name: string
+  attributes: ModelAttributes<Model, Attributes<Model>>
+}
+
+export const entities: ModelConfig[] = []
+
+entities.push({
+  name: 'DrawingDynamic',
+  attributes: {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    userId: {
+      type: DataTypes.UUID,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    history: {
+      type: DataTypes.JSONB,
+    },
+    thumbnail: {
+      type: DataTypes.STRING,
+    },
+  },
+})
 
 export type DrawingInstance = Model<Drawing>
 
