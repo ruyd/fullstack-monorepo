7385e49a9d1fb8ea51a613cd9bdacb8a4da68f84
RD <ruydelgado@gmail.com>
2022-07-15 13:38:25 -0500

    feat: dev

--- server/lazy.js
+++ server/lazy.js
@@ -2,21 +2,48 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const fs = require('fs')
-const spawn = require('cross-spawn')
+const { exec } = require('child_process');
 
-function wire(spawned) {
-  spawned.console.stdout.on('data', (data) => {
+function wire(spw) {
+  spw.stdout.on('data', (data) => {
     console.log(data)
   })
 
-  spawned.console.stderr.on('data', (data) => {
+  spw.stderr.on('data', (data) => {
     console.error(data)
   })
 
-  spawned.console.on('close', (code) => {
+  spw.on('close', (code) => {
     console.log('lazy exit: ' + code)
   })
+  spw.on('error', (code) => {
+    console.error('error: ' + code)
+  })
 }
+
+function wired(text) {
+  const spw = exec(text)
+  wire(spw)
+  return spw
+}
+
+const dev = 'concurrently "npx tsc --watch" "nodemon -q dist/src/index.js"'
+function run() {
+  const arg = process.execArgv[0]
+  wired(arg ? 'npm ' + arg : dev)
+}
+
+//RUN
+
+if (!fs.existsSync('node_modules')) {
+  console.warn('npm install, will load in ~3 minutes...')
+  const spw = wired('npm i')
+  spw.on('close', () => run())
+  return
+} else {
+  console.info('node_modules: check')
+}
+
 
 if (!fs.existsSync('.env')) {
   console.error('Missing .env file for server')
@@ -33,11 +60,3 @@ if (!fs.existsSync('dist')) {
   const result = spawn.sync('npm', ['run', 'build'])
   wire(result)
 }
-
-const dev = spawn.sync(
-  'concurrently',
-  ['"npx tsc --watch"', '"nodemon -q dist/src/index.js"']
-  , { stdio: 'inherit' })
-
-wire(dev)
-
