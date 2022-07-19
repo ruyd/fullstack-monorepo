a03e6993d5af9d211ce8f99baacd1a06ae2be2a6
RD <ruydelgado@gmail.com>
2022-07-18 19:23:41 -0500

    fix: typo

--- client/src/shared/config.ts
+++ client/src/shared/config.ts
@@ -14,7 +14,7 @@ export interface Config {
 
 const config: Config = {
   baseName: process.env.REACT_APP_BASE_NAME || '/fullstack-monorepo',
-  apiUrl: process.env.REACT_APP_BACKEND_API || 'htts://localhost:3001/v1',
+  apiUrl: process.env.REACT_APP_BACKEND_API || 'https://localhost:3001/v1',
   defaultTitle: 'Drawspace',
   defaultColor: 'yellow',
   defaultLineSize: 20,
