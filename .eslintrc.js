93dd67e92803ed9c23eb0c4ac39f4bafd76834a2
RD <rdelgado@inkling.com>
2022-11-10 17:13:01 -0500

    feat: server upgrades

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['dist', 'build', 'node_modules', '*.js'],
+  ignorePatterns: ['dist', 'build', 'node_modules'],
   rules: {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-console': 'warn',
   },
+  "overrides": [
+    {
+      "files": ["*.js"],
+      "rules": {
+        "no-undef": "off",
+        "@typescript-eslint/no-var-requires": "off"
+      }
+    }
+  ]
 }
--- server/src/shared/logger.ts
+++ server/src/shared/logger.ts
@@ -4,17 +4,12 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(winston.format.simple(), winston.format.metadata()),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'trace.log' }),
-  ],
-})
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
     }),
-  )
-}
+    new winston.transports.File({ filename: '_error.log', level: 'error' }),
+    new winston.transports.File({ filename: '_trace.log' }),
+  ],
+})
 
 export default logger
