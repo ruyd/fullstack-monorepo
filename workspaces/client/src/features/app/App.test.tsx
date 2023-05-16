dc89d6eb8ac32b274fd3775487e5d4e7ba9b8a46
RD <ruydelgado@gmail.com>
2023-05-16 16:46:10 -0400

    fix: lint

--- workspaces/client/src/features/app/App.test.tsx
+++ workspaces/client/src/features/app/App.test.tsx
@@ -6,7 +6,7 @@ describe('App', () => {
   test('Root render', async () => {
     const { findAllByRole } = render(<App />)
     await waitFor(() => findAllByRole('progressbar'), {
-      timeout: 10000
+      timeout: 5000
     })
     const appLinks = screen.getAllByText(config.defaultTitle)
     expect(appLinks.length).toBeGreaterThan(0)
