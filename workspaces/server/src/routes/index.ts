38711004c10e010b58ca48e361c3438a70a724fb
RD <ruydelgado@gmail.com>
2023-05-16 23:42:15 -0400

    refactor: stripe api

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import main from './main'
 import profile from './profile'
 import shop from './shop'
+import stripe from './stripe'
 
 const router = express.Router()
 router.use(main)
 router.use(profile)
 router.use(shop)
+router.use(stripe)
 export default router
--- workspaces/server/src/routes/shop/index.ts
+++ workspaces/server/src/routes/shop/index.ts
@@ -1,11 +1,5 @@
 import express from 'express'
 import { addSubscriptionToCart, checkout } from './controller'
-import {
-  stripeCreatePaymentIntent,
-  stripeCreateVerifyIdentitySession,
-  stripeWebHook,
-  syncProductsHandler
-} from './stripe'
 import { capturePaymentHandler, createOrderHandler } from './paypal'
 
 const router = express.Router()
@@ -13,14 +7,6 @@ const router = express.Router()
 router.post('/shop/checkout', checkout)
 
 router.post('/shop/subscribe', addSubscriptionToCart)
-
-router.post('/stripe/payment/intent', stripeCreatePaymentIntent)
-
-router.post('/stripe/identity/start', stripeCreateVerifyIdentitySession)
-
-router.post('/stripe/webhook', express.raw({ type: 'application/json' }), stripeWebHook)
-
-router.get('/stripe/products/sync', syncProductsHandler)
 
 router.post('/paypal/order', createOrderHandler)
 
